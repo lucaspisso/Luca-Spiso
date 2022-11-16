fe46d7d036ecb76860cfd23d1a6726b7f9c3c694
lucaspisso <113534591+lucaspisso@users.noreply.github.com>
2022-11-16 15:29:00 +0100

    Music

---
+++ Music/Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by LUCA SPISSO on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
